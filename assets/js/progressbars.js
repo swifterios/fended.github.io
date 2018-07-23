488d37890ca05fd3067c3c5cbac1d05e53d5ba1a
FENDed <bratap@mail.ru>
2018-07-23 03:05:44 +0300

    Added more sections

--- assets/js/progressbars.js
+++ assets/js/progressbars.js
@@ -1,9 +1,41 @@
 function move() {
-    var elem = document.getElementById("myBar"); 
+    var elem = document.getElementById("myBar-1"); 
     var width = 1;
     var id = setInterval(frame, 10);
     function frame() {
-        if (width >= 100) {
+        if (width >= 90) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
+
+// Bar 2 
+
+function move2() {
+    var elem = document.getElementById("myBar-2"); 
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 84) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
+
+// Bar 3 
+
+function move3() {
+    var elem = document.getElementById("myBar-3"); 
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 70) {
             clearInterval(id);
         } else {
             width++; 
