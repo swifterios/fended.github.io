d3477c54c3c52d56254cb3459174d00e316f44da
FENDed <bratap@mail.ru>
2018-07-27 18:40:47 +0300

    fixes

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-    particlesJS.load('particles-js', 'assets/js/particles.json', function() {
-        console.log('callback - particles.js config loaded');
-      });
-
-});
