83eccd1f533f81f5e5801639df4612e9e277203d
FENDed <39885174+FENDed@users.noreply.github.com>
2019-02-12 12:13:03 +0300

    temp files

---
+++ gametask/assets/js/menu.js
@@ -0,0 +1,11 @@
+let logo_in = document.getElementById('mob_logo');
+let logo_before = document.getElementById('mob_logo_2');
+let menu = document.getElementById('menublock');
+
+logo_in.addEventListener('click', function() {
+    menu.style.display = 'none';
+});
+
+logo_before.addEventListener('click', function() {
+    menu.style.display = 'block';
+});
